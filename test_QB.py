3efc7a9e207874853b5a08824b1e00cea0565c25
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-08-12 15:06:53 -0300

    adding docstring

--- test_QB.py
+++ test_QB.py
@@ -3,7 +3,14 @@ from instrument import Instrument
 import serial
 
 def test_QB():
+    """
+    Checks if temperatures are greater than 80 degrees.
 
+    It simulates 20 times the creation of an instrument for a client, 
+    where he requests the temperature of the equipment, storing it in a list. 
+    At the end, the test checks whether each temperature is greater than 80 degrees; 
+    If so, an error is generated.
+    """
     serial_config = serial.Serial(port='COM3')
     client: Client = Client(ser=serial_config)
 
