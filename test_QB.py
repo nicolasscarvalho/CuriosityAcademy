921e00cb419942d87ebcf47a20fa60452a06c9cb
Nícolas Carvalho <nicolassprofissional@gmail.com>
2023-08-12 14:42:42 -0300

    starting QB test

--- test_QB.py
+++ test_QB.py
@@ -5,6 +5,17 @@ import serial
 def test_QB():
 
     serial_config = serial.Serial(port='COM1')
-
     client: Client = Client(ser=serial_config)
-    instrument: Instrument = Instrument(ser=serial_config)
+
+    temperatures = []
+
+    for i in range(1, 21):
+        instrument: Instrument = Instrument(ser=serial_config)
+
+        client.write_request('TMP')
+        instrument.read_message()
+
+        temperatures.append( int(client.read_response()) )
+
+    for temperature in temperatures:
+        assert temperature > 80
