50a1954f65fdd3c15a21658a6249019d10a1319c
Válcler Manoel Vieira Camelo Maia <valcler.manoel.vieira06@aluno.ifce.edu.br>
2023-08-16 13:57:31 -0300

    test_QC.py implemented

--- test_QC.py
+++ test_QC.py
@@ -4,10 +4,10 @@ from instrument import Instrument
 
 def test_QC():
     """
-    Simulates connecting to an instrument and gets 20 random voltages
+    Simulates connecting to an instrument and gets 20 random voltages in a list.
 
     returns:
-        voltage list(int): List with voltages
+        voltage list(int): List with voltages.
     """
     client: Client = Client(serial.Serial("COM1", 9600, timeout=2))
     instrument: Instrument = Instrument(serial.Serial("COM2", 9600, timeout=2))
@@ -15,15 +15,19 @@ def test_QC():
     voltages = []
 
     for i in range(1, 21):
-        client.write_request("VOL")
-        instrument.read_message()
-        voltage: int = int(client.read_response())
-
-        if voltage == 0:
-            print("Voltage = 0")
-        elif voltage > 190:
-            raise ValueError("Voltage > 190.")
+        client.write_request("VOL")  
+        instrument.read_message()    
+        voltage: int = int(client.read_response())  
 
         voltages.append(voltage)
+
+    print("Voltages:", voltages)
+
+    for voltage in voltages:
+        if voltage == 0:
+            print("Voltage = 0.")
+
+        if voltage > 190:
+            raise ValueError("Voltage > 190.")
 
     return voltages
