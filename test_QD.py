28993912bb84b20a6a0fbe79dbbc675c94d1d799
PedroKeita <82671771+PedroKeita@users.noreply.github.com>
2023-08-15 21:41:24 -0300

    Update test_QD.py

---
+++ test_QD.py
@@ -0,0 +1,24 @@
+from client import Client, serial
+from instrument import Instrument
+
+def test_QD():
+
+    client: Client = Client(serial.Serial("COM1", 9600, timeout=2))
+    instrument: Instrument = Instrument(serial.Serial("COM2", 9600, timeout=2))
+
+    data_dictionary = {
+        "temperature": [instrument.temperature],
+        "voltage": [instrument.voltage],
+        "status": [instrument.status],
+        "type": [instrument.type],
+        "current": [instrument.current],
+    }
+
+
+    return data_dictionary
+data = test_QD()
+print("Dados do Instrumento:")
+
+print(f"{key}: {value}")
+
+
