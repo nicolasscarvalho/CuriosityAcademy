92a1d102ef2e3ec639b7abfa7afc7f9578c74880
Válcler Manoel Vieira Camelo Maia <valcler.manoel.vieira06@aluno.ifce.edu.br>
2023-08-29 18:05:41 -0300

    ValueError test check in test_QC.py

--- tests/test_QC.py
+++ tests/test_QC.py
@@ -4,3 +4,8 @@ def test_voltages_list():
     generated_list_voltages = func_QC()
     assert len(generated_list_voltages) == 20
 
+def test_voltage_error():
+    try:
+        func_QC()
+    except ValueError as e:
+        assert str(e) == "Voltage > 190."
