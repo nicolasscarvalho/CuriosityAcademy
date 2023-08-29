56d704b2441acd6100a2afaf80cae4e1c4ee5934
PedroKeita <82671771+PedroKeita@users.noreply.github.com>
2023-08-29 10:02:43 -0300

    test_QD feito
    feito

--- tests/test_QD.py
+++ tests/test_QD.py
@@ -1,1 +1,25 @@
 from funcs.func_QD import func_QD
+
+def verify_dictionary(dictionary):
+    verify_dictionary_requirements = {
+        "temperature": {"count": 20, "type": int},
+        "voltage": {"count": 20, "type": int},
+        "current": {"count": 20, "type": int},
+        "status": {"count": 5, "type": int},
+        "type": {"count": 5, "type": str},
+    }
+
+    for key, reqs in verify_dictionary_requirements.items():
+        if key not in dictionary or len(dictionary[key]) != reqs["count"]:
+            return False
+
+        if not all(isinstance(value, reqs["type"]) for value in dictionary[key]):
+            return False
+
+    return True
+
+def test_QD():
+    for i in range(1, 11):
+        generated_dict = func_QD()
+
+        assert verify_dictionary(generated_dict) == True
