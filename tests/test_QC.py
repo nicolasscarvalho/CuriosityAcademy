2e51a5539cd925ae7920617b9e870e4bbcb00a3b
Válcler Manoel Vieira Camelo Maia <valcler.manoel.vieira06@aluno.ifce.edu.br>
2023-08-30 20:27:42 -0300

    Removing cache

--- tests/test_QC.py
+++ tests/test_QC.py
@@ -1,6 +1,12 @@
 from funcs.func_QC import func_QC
 
 def test_voltages_list():
+    """
+       Checks if the list has 20 items
+
+       This function tests if the generated list of voltages by func_QC.py
+       has a size of 20 items.
+       """
     try:
         generated_list_voltages = func_QC()
         assert len(generated_list_voltages) == 20
@@ -9,6 +15,12 @@ def test_voltages_list():
 
 
 def test_voltage_error():
+    """
+        Test function for func_QC.py error handling.
+
+        This function tests whether func_QC generates a ValueError with the message
+        "voltage > 190" when a voltage greater than 190 is found in the generated list.
+    """
     try:
         func_QC()
     except ValueError as e:
